baf56c1a0886fd6f50c924223fa9de564f2f120f
ostkaka <john.ostkaka@gmail.com>
2014-09-03 12:48:14 +0200

    Zombies shouldn't destoy stairs anymore.

--- src/io/github/gustav9797/ZombieInvasionMinigame/PathfinderGoal/PathfinderGoalFindBreakBlock.java
+++ src/io/github/gustav9797/ZombieInvasionMinigame/PathfinderGoal/PathfinderGoalFindBreakBlock.java
@@ -38,7 +38,7 @@ public class PathfinderGoalFindBreakBlock extends PathfinderGoal
 
 	@SuppressWarnings("deprecation")
 	private static List<Material> nonBreakableMaterials = new ArrayList<Material>(Arrays.asList(Material.BEDROCK, Material.getMaterial(8), Material.getMaterial(9), Material.GRASS, Material.SAND,
-			Material.AIR, Material.QUARTZ_BLOCK, Material.STONE));
+			Material.AIR, Material.QUARTZ_BLOCK, Material.STONE, Material.WOOD_STAIRS));
 	private static List<Material> naturalMaterials = new ArrayList<Material>(Arrays.asList(Material.GRASS, Material.DIRT, Material.LEAVES));
 	private static List<Material> priorityMaterials = new ArrayList<Material>(Arrays.asList(Material.WOOD_DOOR, Material.IRON_DOOR, Material.TRAP_DOOR, Material.CHEST, Material.THIN_GLASS,
 			Material.STAINED_GLASS, Material.STAINED_GLASS_PANE, Material.GLASS, Material.THIN_GLASS/*
