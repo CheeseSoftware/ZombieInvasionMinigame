9aa37080f77d8cd6b3132b39bd3c027d05f40a7c
Gustav Hammar <gustav9797@hotmail.se>
2014-09-07 09:45:55 +0200

    Don't add itself as dependency..

--- src/io/github/gustav9797/ZombieInvasionMinigame/Entity/ICustomBoss.java
+++ src/io/github/gustav9797/ZombieInvasionMinigame/Entity/ICustomBoss.java
@@ -1,6 +1,6 @@
 package io.github.gustav9797.ZombieInvasionMinigame.Entity;
 
-import io.github.gustav9797.ZombieInvasion.Entity.ICustomMonster;
+import io.github.gustav9797.ZombieInvasionMinigame.Entity.ICustomMonster;
 
 public interface ICustomBoss extends ICustomMonster {
 	
