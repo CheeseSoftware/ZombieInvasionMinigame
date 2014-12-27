702ce51ffe5250c2e0fcd98f64e4aa8c2872338e
Gustav Hammar <gustav9797@hotmail.se>
2014-12-27 15:37:09 +0100

    Test for better zombies

--- src/main/java/io/github/gustav9797/ZombieInvasionMinigame/PathfinderGoal/PathfinderGoalCustomNearestAttackableTarget.java
+++ src/main/java/io/github/gustav9797/ZombieInvasionMinigame/PathfinderGoal/PathfinderGoalCustomNearestAttackableTarget.java
@@ -1,5 +1,6 @@
 package io.github.gustav9797.ZombieInvasionMinigame.PathfinderGoal;
 
+import java.lang.reflect.Field;
 import java.util.Random;
 
 import io.github.gustav9797.State.PlayingState;
@@ -12,64 +13,65 @@ import org.bukkit.event.entity.EntityTargetEvent.TargetReason;
 import net.minecraft.server.v1_8_R1.EntityCreature;
 import net.minecraft.server.v1_8_R1.EntityLiving;
 
-public class PathfinderGoalCustomNearestAttackableTarget extends PathfinderGoalCustomTarget
-{
+public class PathfinderGoalCustomNearestAttackableTarget extends PathfinderGoalCustomTarget {
 
-	private final int attackCheckFrequency;
-	private EntityLiving target;
-	private PlayingState state;
-	private Random r = new Random();
+    private final int attackCheckFrequency;
+    private EntityLiving target;
+    private PlayingState state;
+    private Random r = new Random();
 
-	public PathfinderGoalCustomNearestAttackableTarget(EntityCreature entitycreature, int attackCheckFrequency, PlayingState state)
+    public PathfinderGoalCustomNearestAttackableTarget(EntityCreature entitycreature, int attackCheckFrequency, PlayingState state) {
+	super(entitycreature);
+	this.attackCheckFrequency = attackCheckFrequency;
+	this.state = state;
+	this.a(1);
+    }
+
+    public boolean a() // canExecute
+    {
+	if (this.attackCheckFrequency > 0 && r.nextInt(this.attackCheckFrequency) != 0) {
+	    return false;
+	} else if (state != null
+		&& (this.entity.getGoalTarget() == null || (this.entity.getGoalTarget().getBukkitEntity() instanceof Player && state
+			.isSpectator((Player) this.entity.getGoalTarget().getBukkitEntity()))))
+	// else if(this.entity.getGoalTarget() == null)
 	{
-		super(entitycreature);
-		this.attackCheckFrequency = attackCheckFrequency;
-		this.state = state;
-		this.a(1);
-	}
-
-	public boolean a() // canExecute
-	{
-		if (this.attackCheckFrequency > 0 && r.nextInt(this.attackCheckFrequency) != 0)
-		{
-			return false;
+	    // this.target = (EntityLiving) list.get(0);
+	    // Player[] players = Bukkit.getServer().getOnlinePlayers();
+	    Player closestPlayer = null;
+	    double closestPlayerDistance = Double.MAX_VALUE;
+	    for (Player player : state.getPlayers()) {
+		if (player.getWorld().getName().equals(this.entity.getBukkitEntity().getWorld().getName())) {
+		    double distance = player.getLocation().distance(this.entity.getBukkitEntity().getLocation());
+		    if (distance < closestPlayerDistance && player.getGameMode() == GameMode.SURVIVAL && (state == null || !state.isSpectator(player))) {
+			closestPlayerDistance = distance;
+			closestPlayer = player;
+		    }
 		}
-		else if (state != null
-				&& (this.entity.getGoalTarget() == null || (this.entity.getGoalTarget().getBukkitEntity() instanceof Player && state
-						.isSpectator((Player) this.entity.getGoalTarget().getBukkitEntity()))))
-		// else if(this.entity.getGoalTarget() == null)
-		{
-			// this.target = (EntityLiving) list.get(0);
-			// Player[] players = Bukkit.getServer().getOnlinePlayers();
-			Player closestPlayer = null;
-			double closestPlayerDistance = Double.MAX_VALUE;
-			for (Player player : state.getPlayers())
-			{
-				if (player.getWorld().getName().equals(this.entity.getBukkitEntity().getWorld().getName()))
-				{
-					double distance = player.getLocation().distance(this.entity.getBukkitEntity().getLocation());
-					if (distance < closestPlayerDistance && player.getGameMode() == GameMode.SURVIVAL && (state == null || !state.isSpectator(player)))
-					{
-						closestPlayerDistance = distance;
-						closestPlayer = player;
-					}
-				}
-			}
+	    }
 
-			if (closestPlayer != null)
-			{
-				this.target = ((CraftPlayer) closestPlayer).getHandle();
-				this.entity.setGoalTarget(target, TargetReason.CLOSEST_ENTITY, false);
-				// this.entity.target = this.target;
-				return true;
-			}
+	    if (closestPlayer != null) {
+		this.target = ((CraftPlayer) closestPlayer).getHandle();
+		Field f;
+		try {
+		    f = this.entity.getClass().getField("goalTarget");
+		    f.setAccessible(true);
+		    f.set(this.entity, target);
+		} catch (Exception e) {
+		    e.printStackTrace();
 		}
-		return false;
-	}
 
-	public void c() // setup
-	{
-	    	this.entity.setGoalTarget(target, TargetReason.CLOSEST_ENTITY, false);
-		super.c();
+		// this.entity.setGoalTarget(target, TargetReason.CLOSEST_ENTITY, false);
+		// this.entity.target = this.target;
+		return true;
+	    }
 	}
+	return false;
+    }
+
+    public void c() // setup
+    {
+	this.entity.setGoalTarget(target, TargetReason.CLOSEST_ENTITY, false);
+	super.c();
+    }
 }
